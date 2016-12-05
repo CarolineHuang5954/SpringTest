050379b7ae5b4446d936f239cf1438f33d96bb44
Caroline <huangxm5954@icloud.com>
2016-12-05 16:27:30 +0800

    Spring Resource loader learning update.

--- src/main/java/com/carolearning/resource/SpringResource.java
+++ src/main/java/com/carolearning/resource/SpringResource.java
@@ -19,6 +19,10 @@ public class SpringResource implements ApplicationContextAware{
 		//使用applicationContext.getResource("classpath:config.txt")这种方式
 		//能获取到config文件是因为已经把resources文件夹路径在工程的java build path->source下添加了
 		Resource resource = applicationContext.getResource("classpath:config.txt");
+		//file:方式读取配置文件
+		//Resource resource = applicationContext.getResource("file:C:\\Users\\C5033115\\git\\SpringTest\\src\\main\\resources\\config.txt");
+		//url:方式读取配置文件
+		//Resource resource = applicationContext.getResource("url:http://jingyan.baidu.com/album/ce436649fd82543772afd34f.html");
 		System.out.println(resource.getFilename());
 		System.out.println(resource.contentLength());
 	}
